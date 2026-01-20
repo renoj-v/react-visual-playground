2bbb86531e96eb832fe07191dbd9936740134778
renoj varghese <renoj.creates@gmail.com>
2026-01-19 21:34:04 -0600

    demo star night paritcles

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
         particleSpread={10}
         speed={0.1}
         particleBaseSize={100}
-        moveParticlesOnHover
+        moveParticlesOnHover={false}
         alphaParticles={false}
         disableRotation={false}
         pixelRatio={1}
