369cde0b9ab3a2c059250d2c4b2315a3b7109a5b
renoj varghese <renoj.creates@gmail.com>
2026-02-17 21:47:20 -0600

    testing lennis

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 // import { BackgroundScreen } from './components/BackgroundScreen';
 import MotionDiv from './components/MotionLibrary';
+import { ReactLenis, useLenis } from 'lenis/react';
+
 
 function App() {
+  const lenis = useLenis((lenis) => {
+    // called every scroll
+    console.log(lenis)
+  })
   return (
   <div>
+    <ReactLenis root/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
+    <MotionDiv/>
     <MotionDiv/>
   </div>
   )
