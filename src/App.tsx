8257b33c3351cc1807ec4af557254d3370a5cf3c
renoj varghese <renoj.creates@gmail.com>
2026-02-18 23:10:37 -0600

    removing lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 // import { BackgroundScreen } from './components/BackgroundScreen';
 import MotionDiv from './components/MotionLibrary';
-import { ReactLenis, useLenis } from 'lenis/react';
 
 
 function App() {
-  const lenis = useLenis((lenis) => {
-    // called every scroll
-    console.log(lenis)
-  })
   return (
   <div>
     <ReactLenis root/>
