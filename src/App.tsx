be16fbcf65b3718516c89cec56b9197b21245c28
renoj varghese <renoj.creates@gmail.com>
2026-02-19 20:46:20 -0600

    playing framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 // import { BackgroundScreen } from './components/BackgroundScreen';
-import MotionDiv from './components/MotionLibrary';
+import { MotionDiv } from './components/MotionLibrary';
 
 
 function App() {
   return (
   <div>
-    <ReactLenis root/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
-    <MotionDiv/>
     <MotionDiv/>
   </div>
   )
--- src/components/MotionLibrary.tsx
+++ src/components/MotionLibrary.tsx
@@ -1,25 +1,36 @@
 import { motion } from "motion/react";
-import { useState } from "react";
 
 
 function MotionDiv() {
-    const [bgColor, setBGColor] = useState("#ff0088");
 
     const box = {
         width: 100,
         height: 100,
-        backgroundColor: bgColor,
+        backgroundColor: "#ff0088",
         borderRadius: 5
-    }
+    };
 
-    const timer = setTimeout(() => setTimeout(() => {setBGColor("#00ff88")}), 3000)
     return <motion.div
             style={box}
-            animate={{ rotate: 360 }}
-            transition={{ duration: 1 }}
+            animate={{ 
+                rotate: [-180, 0, 720], 
+                x: [0, null, "50vw", null] }
+            }
+            transition={{ 
+                rotate: {
+                    duration: 5, 
+                    times: [0, 2, 1], 
+                    ease: ["easeIn", "linear", "easeOut"]
+                },
+                x: {
+                    duration: 5, 
+                    times: [0, .4, 1, 1], 
+                    ease: ["easeOut", "easeIn", "easeInOut", "easeOut"]
+                }
+            }}
         />;
 }
 
 
 
-export default MotionDiv;
+export { MotionDiv };
